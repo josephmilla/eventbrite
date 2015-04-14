25f05fdace1c058fbe39e3f7e988cb73aa44e4d7
Joseph Milla <jjtmilla@gmail.com>
2015-04-13 22:13:55 -0500

    Add google maps

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('eventbriteApp')
-  .controller('MainCtrl', function ($scope, $http, $resource, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
@@ -9,55 +9,37 @@ angular.module('eventbriteApp')
       socket.syncUpdates('thing', $scope.awesomeThings);
     });
 
-    // function get_data() {
-    //   $scope.eventbriteAPI = $resource("https://www.eventbriteapi.com/v3/events/?token=BKKRDKVUVRC5WG4HAVLT",
-    //   { callback: "JSON_CALLBACK" },
-    //   { get: { method: "JSON" }});
-    //
-    //   // var events = json.events;
-    //   var events = $scope.eventbriteAPI;
-    //   console.log("asdjlkajsd");
-    //   console.log(events);
-    //   var lats = [];
-    //   var lons = [];
-    //   var coordinates = [];
-    //   for (var i=0; i < events.length; i++) {
-    //     if (events && events[i] && events[i].venue) {
-    //       var venue = events[i].venue;
-    //       var address = venue.address;
-    //       var lat = address.latitude;
-    //       var lon = address.longitude;
-    //       lats.push(lat);
-    //       lons.push(lon);
-    //       coordinates.push(new google.maps.LatLng(lat, lon));
-    //     }
-    //   }
-    //
-    //
-    //   $scope.coordinates = coordinates;
-    // }
-    //
-    // function callUrl(url, async, fn) {
-    //    // console.log(url);
-    //    if (async != false) {
-    //      async = true;
-    //    }
-    //
-    //    var req = new XMLHttpRequest();
-    //    req.open("GET", url, async);
-    //    if (arguments.length == 3) {
-    //      req.onreadystatechange = function () {
-    //        if (req.readyState == 4) {
-    //          fn(req.responseText);
-    //        }
-    //      }
-    //    }
-    //
-    //    try {
-    //      req.send();
-    //    }
-    //    catch(e) {}
-    // }
+    console.log("jasldjlkasjd");
+
+    $http({ method: 'GET', url: 'https://www.eventbriteapi.com/v3/events/?token=BKKRDKVUVRC5WG4HAVLT' }).
+      success(function (data, status, headers, config) {
+        console.log("GET request was successful!");
+        console.log(data);
+
+        var events = data.events;
+        var lats = [];
+        var lons = [];
+        var coordinates = [];
+        for (var i=0; i < events.length; i++) {
+          if (events && events[i] && events[i].venue) {
+            var venue = events[i].venue;
+            var address = venue.address;
+            var lat = address.latitude;
+            var lon = address.longitude;
+            lats.push(lat);
+            lons.push(lon);
+            coordinates.push(new google.maps.LatLng(lat, lon));
+          }
+        }
+
+        console.log(coordinates);
+
+      }).
+      error(function (data, status, headers, config) {
+        console.log("GET request wasn't successful");
+      });
+
+    $scope.coordinates = coordinates;
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
